3dba24a682ced2c40a779bc316f0d7058d7371a4
Ansgar Schmidt <ansgar.schmidt@gmx.net>
2016-09-14 00:28:02 +0200

    Adding Command Header File

---
+++ Arduino/MS_Commands.h
@@ -0,0 +1,27 @@
+#define WAIT_FOR_COMMAND    0
+#define WAIT_FOR_SUBCOMMAND 1
+#define WAIT_FOR_SIZE       2
+#define WAIT_FOR_DATA       3
+
+
+#define     CMD_NETWORK      0x00
+#define SUB_CMD_NETWORK_PING 0x00
+
+#define     CMD_FIRMWARE                      0x10
+#define SUB_CMD_FIRMWARE_GET_FIRMWARE_VERSION 0x20
+#define SUB_CMD_FIRMWARE_GET_UPTIME           0x30
+#define SUB_CMD_FIRMWARE_SET_REBOOT           0x40
+
+#define CMD_SENSOR    0x20
+
+
+#define CMD_MOTOR     0x30
+
+
+#define CMD_LIGHT     0x40
+
+
+#define CMD_HEATER    0x50
+
+
+
