eb64625439f47e21d5e4cbb29868dff033e6f225
Ansgar Schmidt <ansgar.schmidt@gmx.net>
2016-09-14 23:50:05 +0200

    Adding debug data

--- Arduino/Arduino.ino
+++ Arduino/Arduino.ino
@@ -15,36 +15,36 @@ Data_s        DataStorage;
 
 void setDefaults(){
   DataStorage.temperature_outside             = 3.14;
-  DataStorage.temperature_inside              = 3.14;
-  DataStorage.temperature_water               = 3.14;
-  DataStorage.temperature_light1              = 3.14;
-  DataStorage.temperature_light2              = 3.14;
-  DataStorage.temperature_light3              = 3.14;
-  DataStorage.humidity_outside                = 3.14;
-  DataStorage.humidity_inside                 = 3.14;
-  DataStorage.co2                             = 31415;
-  DataStorage.ph                              = 3.14;
-  DataStorage.redox                           = 31415;
+  DataStorage.temperature_inside              = 3.15;
+  DataStorage.temperature_water               = 3.16;
+  DataStorage.temperature_light1              = 3.17;
+  DataStorage.temperature_light2              = 3.18;
+  DataStorage.temperature_light3              = 3.19;
+  DataStorage.humidity_outside                = 3.20;
+  DataStorage.humidity_inside                 = 3.21;
+  DataStorage.co2                             = 314;
+  DataStorage.ph                              = 3.22;
+  DataStorage.redox                           = 3141;
 
   DataStorage.light_speed                     = 50;
-  DataStorage.light_position                  = 0;
-  DataStorage.light_counter                   = 23;
-  DataStorage.light_fan_1_speed               = 0;
-  DataStorage.light_fan_1_desired_temperature = 42;
-  DataStorage.light_fan_2_speed               = 0;
-  DataStorage.light_fan_2_desired_temperature = 42;
-  DataStorage.light_fan_3_speed               = 0;
-  DataStorage.light_fan_3_desired_temperature = 42;
-  DataStorage.hatch                           = 0;
-  DataStorage.outside_fan                     = 0;
-  DataStorage.inside_fan                      = 0;
+  DataStorage.light_position                  = 425;
+  DataStorage.light_counter                   = 2323;
+  DataStorage.light_fan_1_speed               = 51;
+  DataStorage.light_fan_1_desired_temperature = 52;
+  DataStorage.light_fan_2_speed               = 53;
+  DataStorage.light_fan_2_desired_temperature = 54;
+  DataStorage.light_fan_3_speed               = 55;
+  DataStorage.light_fan_3_desired_temperature = 56;
+  DataStorage.hatch                           = 57;
+  DataStorage.outside_fan                     = 58;
+  DataStorage.inside_fan                      = 59;
 
-  DataStorage.ir                              = 0;
-  DataStorage.fs                              = 0;
-  DataStorage.uv                              = 0;
+  DataStorage.ir                              = 60;
+  DataStorage.fs                              = 61;
+  DataStorage.uv                              = 62;
     
-  DataStorage.heater_air                      = 0;
-  DataStorage.heater_water                    = 0;
+  DataStorage.heater_air                      = 63;
+  DataStorage.heater_water                    = 64;
 }
 
 void setup() {
